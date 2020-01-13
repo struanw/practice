dd326ac0060a85613392b6721f2d2c56331dfab9
Struan-Wall <struan03@hotmail.com>
2020-01-13 21:14:46 +1300

    playuing with background image sizes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+     
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+     
     </div>
   );
 }
