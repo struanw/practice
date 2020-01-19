1e9023224226889dd804ba0823dd247978cba06e
Struan-Wall <struan03@hotmail.com>
2020-01-19 19:35:53 +1300

    add mushroom image and write bio

--- src/components.js/AboutPage.js
+++ src/components.js/AboutPage.js
@@ -20,3 +20,16 @@ export default AboutPage
 // load struan can image
 // make background image transparent // or just the bio backgound tansparent
 // make bio pop up from below
+
+
+
+
+
+
+// I am a full stack developer interested in building imursive user experinces.
+// While living in Amsterdam, NL, and spending some time visiting a friend's digital production company, I realised I wanted to work in a creative development environment. 
+// I am engineer and recently completed a web dev boot camp and a compsci certificate. 
+// I am looking for a junior front end role where I can grow as a developer and contribute to a team.
+// This app was built to try out various JS techniques and libraries using React.
+// The photo of me was taken in Christchurch CBD.
+//Hobbies: Surfing, Building a tiny house and rebuilding my Morris Mini
