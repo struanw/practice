cb7a74c8d771bea8da63a5949dbfacbf821d00dc
Struan-Wall <struan03@hotmail.com>
2020-01-28 11:48:16 +1300

    components app.jsx set up

--- client/components.js/App.jsx
+++ client/components.js/App.jsx
@@ -1,4 +1,37 @@
-// import React from 'react';
+import React from 'react'
+
+//import {getWidgets} from '../api'
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+     
+    }
+
+
+
+ 
+
+
+
+
+  render () {
+    return (
+      <div>
+        
+
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
 // //import styled from 'styled-components'
 // import Background from './Background.jsx'
 // //import AboutPage from './AboutPage'
